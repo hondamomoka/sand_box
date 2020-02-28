9874d955bff776d1ce122f1f5439b553d20b501c
hondamomoka <honda.momoka.as@icloud.com>
2020-02-28 13:01:31 +0900

    momoka: 砂の生成方法を改良しました（create_sand）

--- Assets/script/create_sand.cs
+++ Assets/script/create_sand.cs
@@ -7,26 +7,38 @@ public class create_sand : MonoBehaviour {
 
     public GameObject sand;
     GameObject[] obj_sand;
-
+    public float x;
+    public float y;
     // Use this for initialization
     void Start()
     {
         obj_sand = new GameObject[400];
         float count = -2.8f;
-        float y = 0.0f;
+       // float y = 0.0f;
 
-        for (int i = 0; i < 400; i++)
+        Transform mytra = this.transform;
+
+        Vector3 size = mytra.localScale;
+        Vector3 pos = mytra.position;
+
+        x= pos.x - (size.x / 2);
+        y= pos.y - (size.y / 2);
+
+        for (int j = 0; j < (int)size.y * 10; j++)
         {
-            obj_sand[i] = Instantiate(sand, new Vector3(count, y, -0.1f), Quaternion.identity);
-
-            obj_sand[i].transform.parent = GameObject.Find("stage_2").transform;
-
-            count += 0.1f;
-
-            if (count > 2.8)
+            for (int i = 0; i < (int)size.x * 10; i++)
             {
-                count = -2.8f;
-                y += 0.1f;
+                obj_sand[i] = Instantiate(sand, new Vector3(x + ((float)i / 10), y + ((float)j / 10), -0.1f), Quaternion.identity);
+
+                obj_sand[i].transform.parent = GameObject.Find("stage_2").transform;
+
+                //count += 0.1f;
+
+                //if (count > 2.8)
+                //{
+                //    count = -2.8f;
+                //    y += 0.1f;
+                //}
             }
         }
     }
