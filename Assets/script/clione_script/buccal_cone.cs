3689003d5a897f61a23b2be718812bc6ffc6729e
hondamomoka <honda.momoka.as@icloud.com>
2020-05-15 12:27:34 +0900

    momoka:クリオネ完成。

---
+++ Assets/script/clione_script/buccal_cone.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buccal_cone : MonoBehaviour
+{
+    public int count;
+    int count_max;
+    public Material[] material;
+    public GameObject gorl;
+    public hool_2 hool;
+    bool on;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        count_max = 0;
+        on = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(on==true)
+        {
+            if (count > 340)
+            {
+                Destroy(gorl);
+                hool.on = true;
+                on = false;
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("sand_normal"))
+        {
+            count++;
+            other.GetComponent<Renderer>().material = material[1];
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("sand_normal"))
+        {
+            count--;
+            other.GetComponent<Renderer>().material = material[0];
+        }
+    }
+}
---
+++ Assets/script/hool_2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hool_2 : MonoBehaviour
+{
+    public bool on;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        on = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(on)
+        {
+            if (other.gameObject.CompareTag("sand_normal"))
+            {
+                other.gameObject.layer = 19;
+            }
+        }
+
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(on)
+        {
+            if (other.gameObject.CompareTag("sand_normal"))
+            {
+                other.gameObject.layer = 8;
+            }
+        }
+      
+    }
+}
