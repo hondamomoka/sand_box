265332d5476cb628537d2251f46a419b1ec4cb14
hondamomoka <honda.momoka.as@icloud.com>
2020-05-15 18:02:05 +0900

    momoka:カニのステージ完成。

---
+++ Assets/script/crab_script/crab_manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crab_manager : MonoBehaviour
+{
+    public switch_crab sw0;
+    public switch_crab sw1;
+    public bool on;
+    public GameObject scissor1;
+    public GameObject scissor2;
+    public GameObject scissor3;
+    public GameObject scissor4;
+    public Material[] material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        on = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!on)
+        {
+            if (sw0.on && sw1.on)
+            {
+                on = true;
+                scissor1.GetComponent<Renderer>().material = material[0];
+                scissor2.GetComponent<Renderer>().material = material[0];
+                scissor3.GetComponent<Renderer>().material = material[0];
+                scissor4.GetComponent<Renderer>().material = material[0];
+                scissor1.layer = 11;
+                scissor2.layer = 11;
+                scissor3.layer = 11;
+                scissor4.layer = 11;
+            }
+        }
+      
+    }
+}
---
+++ Assets/script/crab_script/switch_crab.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switch_crab : MonoBehaviour
+{
+    public int count;
+    int max_count;
+    public bool on;
+    public Material[] material;
+    public GameObject kani;
+    public switch_crab sw1;
+    public switch_crab sw2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform mytra = this.transform;
+
+        Vector3 size = mytra.localScale;
+
+        max_count = (int)(size.x * size.y * 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!on)
+        {
+            if (count > max_count)
+            {
+                kani.GetComponent<Renderer>().material = material[1];
+                this.GetComponent<Renderer>().material = material[0];
+                on = true;
+                count = 0;
+                sw1.sw_on();
+                sw2.sw_on();
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("sand_normal"))
+        {
+            count++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("sand_normal"))
+        {
+            count--;
+            if(count<0)
+            {
+                count = 0;
+            }
+        }
+    }
+
+    public void sw_on()
+    {
+        on = false;
+        this.GetComponent<Renderer>().material = material[2];
+    }
+}
