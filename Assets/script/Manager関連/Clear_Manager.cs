038538910344c5aadced4277c77b4177958152b8
hondamomoka <honda.momoka.as@icloud.com>
2020-05-23 17:47:11 +0900

    momoka:リスのステージ、背景、パーティクル追加。 あとスイッチBのスクリプト変えたのでエラー出すものあると思います。そこは後々修正していきます。

--- Assets/script/Manager関連/Clear_Manager.cs
+++ Assets/script/Manager関連/Clear_Manager.cs
@@ -8,8 +8,9 @@ public class Clear_Manager : MonoBehaviour
     //何かのobjectに衝突した時の処理
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "player")
+        if (other.gameObject.CompareTag("player"))
         {
+            Debug.Log("入ったよ～");
             SceneManager.LoadScene("Selects");
         }
     }
