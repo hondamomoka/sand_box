1d21fbbdd5dca12b9348c829455bc043464a6fa2
aka <akapikumin232@gmail.com>
2020-05-25 11:50:17 +0900

    ともき:コントローラでステージが回るように

--- Assets/script/rotation.cs
+++ Assets/script/rotation.cs
@@ -4,49 +4,26 @@ using UnityEngine;
 
 public class rotation : MonoBehaviour
 {
-    private bool        contFlag;
-
-    private string[]    stControll;
-
-    void Start()
-    {
-        var controll = Input.GetJoystickNames();
-
-        //if (controll[2] == "")
-        //    contFlag = false;
-        //else
-        //    contFlag = true;
-
-        Debug.Log(controll.Length);
-        //Debug.Log(contFlag);
-    }
-
     void Update()
     {
         Rigidbody rb = this.GetComponent<Rigidbody>();
+        float tri = Input.GetAxis("L_R_Trigger");
 
-        if (contFlag == true)
+        if (tri > 0)
         {
-
-            if (Input.GetKey(KeyCode.Joystick1Button5))
-            {
-                transform.Rotate(0, 0, -10.0f * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.Joystick1Button4))
-            {
-                transform.Rotate(0, 0, 10.0f * Time.deltaTime);
-            }
+            transform.Rotate(0, 0, tri * 30 * Time.deltaTime);
         }
-        else
+        else if (tri < 0)
         {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Rotate(0, 0, -120.0f * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Rotate(0, 0, 120.0f * Time.deltaTime);
-            }
+            transform.Rotate(0, 0, tri * 30 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -30.0f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 30.0f * Time.deltaTime);
         }
     }
 }
