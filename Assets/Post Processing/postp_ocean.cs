1e58527c3483c4449a96e68fc36058af2cdcd050
hondamomoka <honda.momoka.as@icloud.com>
2020-05-27 13:59:39 +0900

    ももか：貝のステージクリアアニメーション追加。

---
+++ Assets/Post Processing/postp_ocean.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class postp_ocean : MonoBehaviour
+{
+    PostProcessVolume posv;
+    Bloom bloom;
+    public bool on;
+    //PostProcessVolume posV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        on = false;
+        bloom = ScriptableObject.CreateInstance<Bloom>();
+        bloom.enabled.Override(true);
+        bloom.intensity.Override(10f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+        {
+            posv = PostProcessManager.instance.QuickVolume(gameObject.layer, 0f, bloom);
+        }
+    }
+}
