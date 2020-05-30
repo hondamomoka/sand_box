fce3b76a3865680e401aff854ce0e4ff5a871864
aka <akapikumin232@gmail.com>
2020-05-31 01:23:42 +0900

    ともき:ペンギン面のwindの音追加

--- Assets/script/wind.cs
+++ Assets/script/wind.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class wind : MonoBehaviour
 {
+    //音をつけるために追加
+    private GameObject audioManager;
+    private Audio_Manager script;
+    [SerializeField] private AudioClip audioClip;
+    private float time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //音をつけるために追加
+        audioManager = GameObject.Find("GameManager");
+        script = audioManager.GetComponent<Audio_Manager>();
+        time = 6.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 2.0f, 0);
+        time += Time.deltaTime;
+        if (time > 5.0f)
+        {
+            script.PlaySE(audioClip);
+            time = 0;
+        }
     }
 }
