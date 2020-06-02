1fd1cf119404d0a50e38c3b8a7e04afd8331fc55
XUWENJIExwj <xwjslivermoon@gmail.com>
2020-06-02 18:15:02 +0900

    許：牛ステージの穴あけ・砂が懐中時計に向かわせる処理

--- Assets/asset/Test/SandsWithBorderDestroy.cs
+++ Assets/asset/Test/SandsWithBorderDestroy.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class SandsWithBorderDestroy : MonoBehaviour
 {
+    GameObject Watch;
+    float degree;
+    float move_index;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Watch = GameObject.Find("PocketWatchZone");
+        move_index = 16.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.layer == 28)
+        {
+            transform.position = new Vector3(
+                transform.position.x + Mathf.Cos(degree) / move_index,
+                transform.position.y + Mathf.Sin(degree) / move_index,
+                Watch.transform.position.z);
+            transform.localScale *= 1.004f;
 
+            if(move_index > 1.0f)
+            {
+                move_index -= 0.1f;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("stage_border"))
+        if (other.gameObject == Watch)
         {
             Destroy(this.gameObject);
         }
+
+        if (other.gameObject.CompareTag("stage_border"))
+        {
+            // layer: sands_and_watch
+            gameObject.layer = 28;
+
+            transform.parent = null;
+
+            degree = Mathf.Atan2(
+                Watch.transform.position.y + Random.Range(-Watch.transform.lossyScale.z, Watch.transform.lossyScale.z) / 3 - transform.position.y,
+                Watch.transform.position.x + Random.Range(-Watch.transform.lossyScale.x, Watch.transform.lossyScale.x) / 3 - transform.position.x);
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (other.gameObject.CompareTag("stage_border"))
+        //{
+        //    // layer: sands_and_watch
+        //    gameObject.layer = 28;
+
+        //    degree = Mathf.Atan2(
+        //        Watch.transform.position.y - transform.position.y,
+        //        Watch.transform.position.x - transform.position.x);
+        //    GetComponent<Rigidbody>().useGravity = false;
+        //}
     }
 }
