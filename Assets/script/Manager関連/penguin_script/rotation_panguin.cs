68cacbcb8fef95355cc23a9bbaaa076219aefe7f
XUWENJIExwj <xwjslivermoon@gmail.com>
2020-06-09 10:20:59 +0900

    許：ペンギンのコインの浮力の調整・回転バグの修正

--- Assets/script/Manager関連/penguin_script/rotation_panguin.cs
+++ Assets/script/Manager関連/penguin_script/rotation_panguin.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class rotation_panguin : MonoBehaviour
 {
-
-    float rot;
+    public float rot_limit;
+    public float deviation;
 
     // Start is called before the first frame update
     void Start()
     {
-        rot = 0.0f;
+
     }
 
     // Update is called once per frame
@@ -19,35 +19,32 @@ public class rotation_panguin : MonoBehaviour
         Rigidbody rb = this.GetComponent<Rigidbody>();
         float tri = Input.GetAxis("L_R_Trigger");
 
-        if (rot>-20.0f)
+        if (tri > 0)
         {
-            if (tri > 0)
-            {
-                rot += tri * -44.5f * Time.deltaTime;
-                transform.Rotate(0, 0, -44.5f * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rot -= 33.4f * Time.deltaTime;
-                transform.Rotate(0, 0, -33.4f * Time.deltaTime);
-            }
+            transform.Rotate(0, 0, -44.5f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -33.4f * Time.deltaTime);
         }
 
-        if (rot<20.0f)
+        if (tri < 0)
         {
-            if (tri < 0)
-            {
-                rot -= tri * 44.5f * Time.deltaTime;
-                transform.Rotate(0, 0, tri * -44.5f * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rot += 33.4f * Time.deltaTime;
-                transform.Rotate(0, 0, 33.4f * Time.deltaTime);
-            }
+            transform.Rotate(0, 0, tri * -44.5f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 33.4f * Time.deltaTime);
         }
 
+        if(transform.eulerAngles.z > rot_limit && transform.eulerAngles.z < rot_limit + deviation)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, rot_limit);
+        }
 
-
+        if(transform.eulerAngles.z < 360.0f - rot_limit && transform.eulerAngles.z > 360.0f - (rot_limit + deviation))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 360.0f - rot_limit);
+        }
     }
 }
