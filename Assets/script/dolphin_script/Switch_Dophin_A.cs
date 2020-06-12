fdfcd7580ec97c6a167ed9c79540cd7265c159a2
XUWENJIExwj <xwjslivermoon@gmail.com>
2020-06-12 17:10:37 +0900

    許：イルカの砂の大きさの調整

---
+++ Assets/script/dolphin_script/Switch_Dophin_A.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Dophin_A : MonoBehaviour
+{
+    public GameObject wall;
+    public Material[] material;
+    public swichEFonly_cobra effect;
+    public int count = 0;
+    bool on = false;
+
+    //音をつけるために追加
+    private GameObject audioManager;
+    private Audio_Manager script;
+    [SerializeField] private AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //音をつけるために追加
+        audioManager = GameObject.Find("GameManager");
+        script = audioManager.GetComponent<Audio_Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(10f * Time.deltaTime, 0, 20f * Time.deltaTime);
+
+        if (count > 40)
+        {
+            on = true;
+            count = 0;
+            this.GetComponent<Renderer>().material = material[0];
+            effect.playPS();
+            script.PlaySE(audioClip);
+
+            Destroy(wall.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (on == false)
+        {
+            if (other.gameObject.CompareTag("sand_normal") || other.gameObject.CompareTag("sand_float"))
+            {
+                count++;
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (on == false)
+        {
+            if (other.gameObject.CompareTag("sand_normal") || other.gameObject.CompareTag("sand_float"))
+            {
+                count--;
+            }
+        }
+    }
+}
---
+++ Assets/script/dolphin_script/Switch_Dophin_Goal.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Dophin_Goal : MonoBehaviour
+{
+    public int count;
+    //int max_count;
+    public bool on;
+    public Material[] material;
+    public GameObject cube;
+    public swichEFonly_cobra effect;
+    public ParticleSystem ps1;
+
+    //音をつけるために追加
+    private GameObject audioManager;
+    private Audio_Manager script;
+    [SerializeField] private AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps1.Stop();
+        Transform mytra = this.transform;
+
+        Vector3 size = mytra.localScale;
+
+        //max_count = (int)(size.x * size.y * 100);
+        on = false;
+
+        //音をつけるために追加
+        audioManager = GameObject.Find("GameManager");
+        script = audioManager.GetComponent<Audio_Manager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(10f * Time.deltaTime, 0, 20f * Time.deltaTime);
+
+        if (!on)
+        {
+            if (count > 46)
+            {
+                on = true;
+                cube.GetComponent<Renderer>().material = material[1];
+                cube.layer = 14;
+                cube.tag = "Untagged";
+                this.GetComponent<Renderer>().material = material[0];
+                effect.playPS();
+                ps1.Play();
+                script.PlaySE(audioClip);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!on)
+        {
+            if (other.gameObject.CompareTag("sand_normal"))
+            {
+                count++;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!on)
+        {
+            if (other.gameObject.CompareTag("sand_normal"))
+            {
+                count--;
+
+                if (count < 0)
+                {
+                    count = 0;
+                }
+            }
+        }
+    }
+}
