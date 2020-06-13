66b1c973dfe336ffdc342390e1d69228190ec1ed
XUWENJIExwj <xwjslivermoon@gmail.com>
2020-06-13 17:12:41 +0900

    許：ウサギステージをクリアしたとき、出口の方向によって、コインがステージに落ちることがあるので、その時のコインを消す処理の追加(パーティクル付き、スイッチを押したときのパーティクルを使用)

---
+++ Assets/asset/Test/PlayerDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroy : MonoBehaviour
+{
+    public ParticleSystem ps1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("stage_border"))
+        {
+            Instantiate(ps1, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
